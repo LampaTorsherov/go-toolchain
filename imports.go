661632f39ff8472b3ad6de510abb3527a8e29e8b
Лампа Торшеров <friedrih.gaffen@gmail.com>
2026-04-23 22:47:32 +0300

    Update imports.go

--- imports.go
+++ imports.go
@@ -1,7 +1,14 @@
 package main
 
 import (
-  
+    _ "github.com/google/uuid"
+    _ "github.com/jcmturner/gokrb5/v8"
+    _ "github.com/joomcode/errorx"
+    _ "github.com/klauspost/shutdown2"
+    _ "github.com/uptrace/bun"
+    _ "github.com/uptrace/bun/dialect/sqlitedialect"
+    _ "golang.org/x/sync/errgroup"
+    _ "modernc.org/sqlite"
 )
 
 func main() {}
